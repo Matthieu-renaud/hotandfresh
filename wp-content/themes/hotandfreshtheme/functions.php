f895a54cb23f73464f7f18de7a4855d08f04a73a
Matthieu RENAUD <matthieurenaud07@gmail.com>
2021-06-28 14:23:06 +0200

    modif

--- wp-content/themes/hotandfreshtheme/functions.php
+++ wp-content/themes/hotandfreshtheme/functions.php
@@ -7,20 +7,10 @@ function montheme_supports()
 
 function montheme_register_assets()
 {
-  // Déclaration de la librairie d'icônes Font Awesome
-  wp_enqueue_style(
-    'fontAwesome',
-    get_template_directory_uri() . '/assets/css/fontawesome-free-5.15.3/css/all.min.css',
-    array(),
-    '5.15.3'
-  );
-
   // Déclaration du fichier de style principal
   wp_enqueue_style(
     'main',
     get_template_directory_uri() . '/assets/css/main.css',
-    array('fontAwesome'),
-    '1.0'
   );
 }
 
