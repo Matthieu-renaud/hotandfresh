06ae49ad70cbf3b071b8ec1404d2e66f4e876b7b
Matthieu RENAUD <matthieurenaud07@gmail.com>
2021-06-28 12:08:15 +0200

    ajout style, et ça marche

--- wp-content/themes/hotandfreshtheme/functions.php
+++ wp-content/themes/hotandfreshtheme/functions.php
@@ -7,8 +7,21 @@ function montheme_supports()
 
 function montheme_register_assets()
 {
-  wp_register_style('hotandfresh', './assets/css/hotandfresh_style.css');
-  wp_enqueue_style('hotandfresh');
+  // Déclaration de la librairie d'icônes Font Awesome
+  wp_enqueue_style(
+    'fontAwesome',
+    get_template_directory_uri() . '/assets/css/fontawesome-free-5.15.3/css/all.min.css',
+    array(),
+    '5.15.3'
+  );
+
+  // Déclaration du fichier de style principal
+  wp_enqueue_style(
+    'main',
+    get_template_directory_uri() . '/assets/css/main.css',
+    array('fontAwesome'),
+    '1.0'
+  );
 }
 
 add_action('after_setup_theme', 'montheme_supports');
